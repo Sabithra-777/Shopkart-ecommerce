3b5f54e4384156aa47befbb9966c95a2dd7661a0
Sabithra-777 <sabithra.m2024cse@sece.ac.in>
2025-06-02 11:12:45 +0530

    Delete apps.py

--- apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShopConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shop'
-    
-    # Removed the ready method that imported signals
